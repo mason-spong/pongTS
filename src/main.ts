ddbb4be4303efd0d5bde4d0fe4d53ab4f64d28b6
Mason Spong <mason.spong@gmail.com>
2022-12-18 17:45:56 -0700

    Add simple code to draw line in canvas

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { square } from "./mathUtils";
 
-const canvas = document.getElementById("canvas");
+const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 console.log(`canvas: ${canvas?.id}`);
 console.log(`Square(2): ${square(2)}`);
+const ctx = canvas.getContext("2d");
+ctx?.moveTo(0, 0);
+ctx?.lineTo(800, 600);
+ctx?.stroke();
